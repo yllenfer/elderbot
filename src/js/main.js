5897071c3e2980c89700e01083563a2bf177f881
Yllen Fernandez <yllenfernandez@gmail.com>
2024-03-03 23:27:19 +0100

    Add Firebase SDK imports and initialize Firebase

--- src/js/main.js
+++ src/js/main.js
@@ -8,6 +8,27 @@ import { createClient } from "@supabase/supabase-js";
 import { RunnableSequence, RunnablePassthrough } from '@langchain/core/runnables';
 import { v4 as uuidv4 } from 'uuid';
 import {dotenv} from 'dotenv';
+ // Import the functions you need from the SDKs you need
+ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
+ import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-analytics.js";
+ // TODO: Add SDKs for Firebase products that you want to use
+ // https://firebase.google.com/docs/web/setup#available-libraries
+
+ // Your web app's Firebase configuration
+ // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+ const firebaseConfig = {
+   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+   projectId: import.meta.env.VITE_PROJECT_ID,
+   storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+   messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER,
+   appId: import.meta.env.VITE_APP_ID,
+   measurementId: import.meta.env.VITE_MEASURE_ID
+ };
+
+ // Initialize Firebase
+ const app = initializeApp(firebaseConfig);
+ const analytics = getAnalytics(app);
 
 
 
